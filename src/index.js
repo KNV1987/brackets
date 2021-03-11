37c5682458498874f565e705d27184fdcbc2774f
KNV 1987 <Kalibabchuk@yandex.ru>
2021-03-11 23:12:11 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    str = str.split("");
+    let objBrackets = {};
+    let acc = [];
+    let openIndex;
+    let closeIndex;
+    if (str.length % 2 !== 0) return false;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        objBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+    for (let j = 0; j < str.length; j++) {
+        openIndex = Object.keys(objBrackets).indexOf(str[j]);
+        if (
+            openIndex > -1 &&
+            acc.indexOf(str[j]) > -1 &&
+            objBrackets[str[j]] === str[j]
+        ) {
+            closeIndex = Object.values(objBrackets).indexOf(str[j]);
+            if (
+                closeIndex > -1 &&
+                objBrackets[acc[acc.length - 1]] === str[j]
+            ) {
+                acc.pop();
+            }
+            continue;
+        }
+        if (openIndex > -1) {
+            acc.push(str[j]);
+            continue;
+        }
+        closeIndex = Object.values(objBrackets).indexOf(str[j]);
+        if (closeIndex > -1 && objBrackets[acc[acc.length - 1]] === str[j]) {
+            acc.pop();
+        }
+    }
+    console.log(acc);
+    return acc.length === 0;
+};
